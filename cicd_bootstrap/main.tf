f7621abc3bc5c023ec43a0fd930f4ebe334dcabd
PCzerska <pczerska@DESKTOP-SORQQO0.localdomain>
2025-04-03 20:58:45 +0200

    Updated configuration and added terraform-docs package

--- cicd_bootstrap/main.tf
+++ cicd_bootstrap/main.tf
@@ -2,10 +2,10 @@ data "google_service_account" "iac" {
   account_id = var.iac_service_account
 }
 
+# Zmiana nazwy workload_identity_pool_id na unikalną, aby uniknąć konfliktu z istniejącym zasobem
 resource "google_iam_workload_identity_pool" "tbd-workload-identity-pool" {
-  workload_identity_pool_id = "github-actions-pool"
+  workload_identity_pool_id = "github-actions-pool-unique" # Unikalna nazwa, aby uniknąć konfliktu
 }
-
 
 resource "google_iam_workload_identity_pool_provider" "tbd-workload-identity-provider" {
   #checkov:skip=CKV_GCP_125: "Ensure GCP GitHub Actions OIDC trust policy is configured securely"
@@ -13,13 +13,17 @@ resource "google_iam_workload_identity_pool_provider" "tbd-workload-identity-pro
   workload_identity_pool_provider_id = "github-actions-provider"
   display_name                       = "GitHub provider"
   description                        = "GitHub identity pool provider for CI/CD purposes"
+
+  # Poprawiona składnia - użycie = zamiast :
   attribute_mapping = {
-    "google.subject" : "assertion.sub"
-    "attribute.repository" : "assertion.repository"
-    "attribute.org"  = "assertion.repository_owner"
-    "attribute.refs" = "assertion.ref"
+    "google.subject"       = "assertion.sub"
+    "attribute.repository" = "assertion.repository"
+    "attribute.org"        = "assertion.repository_owner"
+    "attribute.refs"       = "assertion.ref"
   }
+
   attribute_condition = "attribute.org == \"${var.github_org}\""
+
   oidc {
     issuer_uri = "https://token.actions.githubusercontent.com"
   }
