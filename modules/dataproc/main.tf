b2ec954756d78db8b316c337239dfaa8d0008f6f
BigCatStudio <101984342+BigCatStudio@users.noreply.github.com>
2025-04-11 21:42:03 +0200

    Update main.tf - task 12 preemptible 1

--- modules/dataproc/main.tf
+++ modules/dataproc/main.tf
@@ -49,10 +49,9 @@ resource "google_dataproc_cluster" "tbd-dataproc-cluster" {
       }
     }
 
-  secondary_worker_config {
-      num_instances    = 1
-      is_preemptible   = true
-      machine_type     = var.machine_type
+    preemptible_worker_config {
+      num_instances = 1
+      machine_type  = var.machine_type
       disk_config {
         boot_disk_type    = "pd-standard"
         boot_disk_size_gb = 100
